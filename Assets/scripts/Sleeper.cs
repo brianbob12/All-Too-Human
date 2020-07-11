363f701686f0dfcda452275e7b51504ad0540e4e
Cyrus Signer <japaneserhino@gmail.com>
2020-07-11 20:25:20 +0100

    New towers and animations

---
+++ Assets/scripts/Sleeper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleeper : Tower {
+    private bool attacking=false;
+    private int attackFrame=0;
+    private int attackingFrames = 8;
+    private float timePerFame = 0.1f;
+ 
+    private float lastTime;
+    public Sprite[] attackSequence;
+    //sleeping
+    private bool sleeping = false;
+    private int sleepFrame = 0;
+    private int sleepingFrames = 5;
+    public Sprite[] sleepSequence;
+
+    protected override void Start()
+    {
+        base.Start();
+        spr.sprite = stand1;
+    }
+
+    protected override void fixedUpdateCall()
+    {
+        if (!levelManager.getPause())
+        {
+            targetSelect();
+
+            //attack timer
+            checkFire();
+
+            if (attacking) {
+                if (getTime() - lastTime > timePerFame) {
+                    attackFrame += 1;
+                    if (attackFrame >= attackingFrames)
+                    {
+                        attacking = false;
+                        spr.sprite = stand1;
+                    }
+                    else
+                    {
+                        spr.sprite = attackSequence[attackFrame];
+                        lastTime = getTime();
+                    }
+                }
+            }
+        }
+    }
+
+    protected override void inActiveUpdate()
+    {
+        base.inActiveUpdate();
+        if (sleeping)
+        {
+            if (getTime() - lastTime > timePerFame)
+            {
+                sleepFrame += 1;
+                sleepFrame %= sleepingFrames;
+                spr.sprite = sleepSequence[attackFrame];
+                lastTime = getTime();
+            }
+        }
+    }
+
+    protected override void fire()
+    {
+        target.damage(damage);
+        if (!attacking) {
+            attacking = true;
+            attackFrame = 0;
+            lastTime = getTime();
+            spr.sprite = attackSequence[0];
+        }
+    }
+
+    private float getTime()
+    {//returns a float of seconds since jan 1 1970
+
+        float time = (float)((DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds - 1594000000);
+        return time;
+    }
+
+    public void goToSleep() {
+        setActive(false);
+
+    }
+
+    public void wake() {//rudly awakens robot from gentle slumber
+        sleeping = false;
+        spr.sprite = stand1;
+    }
+
+}
--- Assets/scripts/Tower.cs
+++ Assets/scripts/Tower.cs
@@ -8,7 +8,6 @@ public class Tower : MonoBehaviour {
     public LevelManager levelManager;
     public CircleCollider2D rangeCollider;
     
-    public float maxHealth = 100;
     public float bulletSpeed;
     public int fireingRate=10;//number of fixedUpdates between shots
     public int damage = 0;//damage delt per shot
@@ -37,9 +36,9 @@ public class Tower : MonoBehaviour {
     //private variables
     private bool active=true;//whether or not this Tower is active
     private Rigidbody2D rb;
-    private Enemy target;//currently selected target
+    protected Enemy target;//currently selected target
     private float fireingTimer = 0;//logs fixedUPdates since last fired
-    private SpriteRenderer spr;
+    protected SpriteRenderer spr;
     private GameObject activeFirePoint;//the source of bullets
 
     //getters and setters}
@@ -62,199 +61,45 @@ public class Tower : MonoBehaviour {
     public attackSetting setting = attackSetting.CLOSE;
 
     // Use this for initialization
-    void Start () {
+    protected virtual void Start () {
 		rb=this.GetComponent<Rigidbody2D>();
         spr = GetComponentInChildren<SpriteRenderer>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
 
 
     private void FixedUpdate()
     {
+        if (active)
+        {
+            fixedUpdateCall();//for overrideing
+        }
+        else {
+            inActiveUpdate();
+        }
+    }
+
+    protected virtual void fixedUpdateCall() {
         if (!levelManager.getPause())
         {
-            //select target
-            target = null;
-
-            //this part is for removing dead enemies
-            List<Enemy> enemiesCopy = new List<Enemy>();
-            foreach (Enemy en in enemies)
-            {
-                enemiesCopy.Add(en);
-            }
-
-            if (enemies.Count != 0)
-            {
-                foreach (Enemy en in enemiesCopy)
-                {
-                    if (en == null)
-                    {//en has died
-                     //remove en from master list
-                        enemies.Remove(en);
-                        continue;
-                    }
-
-                    if (target == null)
-                    {
-                        target = en;
-                        continue;
-                    }
-                    if (this.setting == attackSetting.AHEAD)
-                    {
-                        if (en.aheadScore() > target.aheadScore())
-                        {
-                            target = en;
-                        }
-                    }
-                    else if (this.setting == attackSetting.BEHIND)
-                    {
-                        if (en.aheadScore() < target.aheadScore())
-                        {
-                            target = en;
-                        }
-                    }
-                    else if (this.setting == attackSetting.STRONG)
-                    {
-                        if (en.strengthScore() > target.strengthScore())
-                        {
-                            target = en;
-                        }
-                    }
-                    else if (this.setting == attackSetting.WEAK)
-                    {
-                        if (en.strengthScore() < target.strengthScore())
-                        {
-                            target = en;
-                        }
-                    }
-                    else if (this.setting == attackSetting.FAR)
-                    {
-                        if (en.closeScore(this.gameObject) > target.closeScore(this.gameObject))
-                        {
-                            target = en;
-                        }
-                    }
-                    else if (this.setting == attackSetting.CLOSE)
-                    {
-                        if (en.closeScore(this.gameObject) < target.closeScore(this.gameObject))
-                        {
-                            target = en;
-                        }
-                    }
-                }
-            }
+            targetSelect();
             //point towards target
 
             //sprites and firepoints
             if (target != null)
             {
-                //targetting
-                float angle = Vector2.SignedAngle(transform.up, (Vector2)(target.transform.position - transform.position));
-                if (angle < -157.5)
-                {
-                    spr.sprite = stand5;
-                    activeFirePoint = firePoint5;
-                }
-                else if (angle < -112.5)
-                {
-                    spr.sprite = stand6;
-                    activeFirePoint = firePoint6;
-                }
-                else if (angle < -67.5)
-                {
-                    spr.sprite = stand7;
-                    activeFirePoint = firePoint7;
-                }
-                else if (angle < -22.5)
-                {
-                    spr.sprite = stand8;
-                    activeFirePoint = firePoint8;
-                }
-                else if (angle < 22.5)
-                {
-                    spr.sprite = stand1;
-                    activeFirePoint = firePoint1;
-                }
-                else if (angle < 67.5)
-                {
-                    spr.sprite = stand2;
-                    activeFirePoint = firePoint2;
-                }
-                else if (angle < 112.5)
-                {
-                    spr.sprite = stand3;
-                    activeFirePoint = firePoint3;
-                }
-                else if (angle < 157.5)
-                {
-                    spr.sprite = stand4;
-                    activeFirePoint = firePoint4;
-                }
-                else
-                {
-                    spr.sprite = stand5;
-                    activeFirePoint = firePoint5;
-                }
+                pointToTarget();
             }
 
             //attack timer
-            if (fireingTimer >= fireingRate)
-            {
+            checkFire();
 
-                if (target != null)
-                {
-                    fireingTimer = 0;
-                    //fire
-                    target.damage(damage);
-                    //TODO
-                    //play fireing animation
-                    //dispatch weapon
 
-                    //old system
-                    
-                    Vector2 fireVector = ((Vector2)target.transform.position+target.getGoing()*35) - (Vector2)activeFirePoint.transform.position;
-                    fireVector /= fireVector.magnitude;//normalize
-                    
-
-                    //new system
-                    /*
-                    Vector2 alpha = (Vector2)activeFirePoint.transform.position - (Vector2)target.transform.position;
-                    alpha /= alpha.magnitude;
-                    alpha *= bulletSpeed;
-                    alpha *= ((Vector2)target.transform.position).magnitude / target.getGoing().magnitude;
-                    //convergance
-                    for (int i = 0; i < 50;i++) {
-                        Vector2 newAlpha= (Vector2)activeFirePoint.transform.position - alpha;
-                        newAlpha /= newAlpha.magnitude;
-                        newAlpha*= bulletSpeed;
-                        newAlpha *= alpha.magnitude / target.getGoing().magnitude;
-                        alpha = newAlpha;
-                    }
-
-                    Vector2 fireVector = alpha;
-                    */
-
-                    Bullet placed = Instantiate(projectile, activeFirePoint.transform.position, Quaternion.identity).GetComponent<Bullet>();
-                    //setup
-                    placed.target = target.gameObject;
-                    placed.motionVector = fireVector;
-                    placed.speed = bulletSpeed;
-                }
-                else
-                {
-                    fireingTimer += 1;
-                }
-            }
-            else {
-                fireingTimer += 1;
-            }
-
-            
         }
+    }
+
+    protected virtual void inActiveUpdate() {
+
     }
 
     //manage enemeis list
@@ -277,4 +122,184 @@ public class Tower : MonoBehaviour {
         }
     }
 
+    protected void checkFire()
+    {
+        if (fireingTimer >= fireingRate)
+        {
+
+            if (target != null)
+            {
+                fireingTimer = 0;
+                //fire
+                fire();
+            }
+            else
+            {
+                fireingTimer += 1;
+            }
+        }
+        else
+        {
+            fireingTimer += 1;
+        }
+    }
+    protected virtual void fire()
+    {
+        target.damage(damage);
+        //TODO
+        //play fireing animation
+        //dispatch weapon
+
+        //old system
+
+        Vector2 fireVector = ((Vector2)target.transform.position + target.getGoing() * 35) - (Vector2)activeFirePoint.transform.position;
+        fireVector /= fireVector.magnitude;//normalize
+
+
+        //new system
+        /*
+        Vector2 alpha = (Vector2)activeFirePoint.transform.position - (Vector2)target.transform.position;
+        alpha /= alpha.magnitude;
+        alpha *= bulletSpeed;
+        alpha *= ((Vector2)target.transform.position).magnitude / target.getGoing().magnitude;
+        //convergance
+        for (int i = 0; i < 50;i++) {
+            Vector2 newAlpha= (Vector2)activeFirePoint.transform.position - alpha;
+            newAlpha /= newAlpha.magnitude;
+            newAlpha*= bulletSpeed;
+            newAlpha *= alpha.magnitude / target.getGoing().magnitude;
+            alpha = newAlpha;
+        }
+
+        Vector2 fireVector = alpha;
+        */
+
+        Bullet placed = Instantiate(projectile, activeFirePoint.transform.position, Quaternion.identity).GetComponent<Bullet>();
+        //setup
+        placed.target = target.gameObject;
+        placed.motionVector = fireVector;
+        placed.speed = bulletSpeed;
+    }
+    protected void targetSelect() {
+        //select target
+        target = null;
+
+        //this part is for removing dead enemies
+        List<Enemy> enemiesCopy = new List<Enemy>();
+        foreach (Enemy en in enemies)
+        {
+            enemiesCopy.Add(en);
+        }
+
+        if (enemies.Count != 0)
+        {
+            foreach (Enemy en in enemiesCopy)
+            {
+                if (en == null)
+                {//en has died
+                 //remove en from master list
+                    enemies.Remove(en);
+                    continue;
+                }
+
+                if (target == null)
+                {
+                    target = en;
+                    continue;
+                }
+                if (this.setting == attackSetting.AHEAD)
+                {
+                    if (en.aheadScore() > target.aheadScore())
+                    {
+                        target = en;
+                    }
+                }
+                else if (this.setting == attackSetting.BEHIND)
+                {
+                    if (en.aheadScore() < target.aheadScore())
+                    {
+                        target = en;
+                    }
+                }
+                else if (this.setting == attackSetting.STRONG)
+                {
+                    if (en.strengthScore() > target.strengthScore())
+                    {
+                        target = en;
+                    }
+                }
+                else if (this.setting == attackSetting.WEAK)
+                {
+                    if (en.strengthScore() < target.strengthScore())
+                    {
+                        target = en;
+                    }
+                }
+                else if (this.setting == attackSetting.FAR)
+                {
+                    if (en.closeScore(this.gameObject) > target.closeScore(this.gameObject))
+                    {
+                        target = en;
+                    }
+                }
+                else if (this.setting == attackSetting.CLOSE)
+                {
+                    if (en.closeScore(this.gameObject) < target.closeScore(this.gameObject))
+                    {
+                        target = en;
+                    }
+                }
+            }
+        }
+    }
+
+    private void pointToTarget() {
+        //targetting
+        float angle = Vector2.SignedAngle(transform.up, (Vector2)(target.transform.position - transform.position));
+        if (angle < -157.5)
+        {
+            spr.sprite = stand5;
+            activeFirePoint = firePoint5;
+        }
+        else if (angle < -112.5)
+        {
+            spr.sprite = stand6;
+            activeFirePoint = firePoint6;
+        }
+        else if (angle < -67.5)
+        {
+            spr.sprite = stand7;
+            activeFirePoint = firePoint7;
+        }
+        else if (angle < -22.5)
+        {
+            spr.sprite = stand8;
+            activeFirePoint = firePoint8;
+        }
+        else if (angle < 22.5)
+        {
+            spr.sprite = stand1;
+            activeFirePoint = firePoint1;
+        }
+        else if (angle < 67.5)
+        {
+            spr.sprite = stand2;
+            activeFirePoint = firePoint2;
+        }
+        else if (angle < 112.5)
+        {
+            spr.sprite = stand3;
+            activeFirePoint = firePoint3;
+        }
+        else if (angle < 157.5)
+        {
+            spr.sprite = stand4;
+            activeFirePoint = firePoint4;
+        }
+        else
+        {
+            spr.sprite = stand5;
+            activeFirePoint = firePoint5;
+        }
+    }
 }
